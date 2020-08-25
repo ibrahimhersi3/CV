ce2f059d4acd2a67ac8d193b5eb2039f1dc9c78e
ibrahimhersi3 <ibrahim.hersi.2@gmail.com>
2020-08-25 18:24:29 +0200

    adding npm web

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.database import engine, Base
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.models.profile import Profile
 from app.models.project import Project
@@ -8,6 +9,17 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 from app.routes import profile
 from app.routes import project
