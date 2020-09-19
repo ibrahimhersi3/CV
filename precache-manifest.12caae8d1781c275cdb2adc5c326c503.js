29256cc920341e090772d1be3ad3c6b78d829552
ibrahimhersi3 <ibrahim.hersi.2@gmail.com>
2020-09-19 23:35:30 +0200

    Updates

---
+++ precache-manifest.12caae8d1781c275cdb2adc5c326c503.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e7d8f61eca14e5699e060fe2e292c97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ca760c0b356442bbbb0",
+    "url": "/static/css/main.fa5bc276.chunk.css"
+  },
+  {
+    "revision": "4c3325a7e79c86ed25c7",
+    "url": "/static/js/2.daf8ed36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.daf8ed36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ca760c0b356442bbbb0",
+    "url": "/static/js/main.4b860cca.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  }
+]);
